5a65fe1b96e142689077d634943aa3d2a5efe81a
keh9104 <61343846+keh9104@users.noreply.github.com>
2020-06-27 15:12:23 +0700

    날씨 앱 커밋

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API = {
+  key: "7200fa31ca289c0f3bc09aacf45b9035",
+  url: "http://api.openweathermap.org/data/2.5/",
+};
+
+const searchbox = document.querySelector(".search-box");
+searchbox.addEventListener("keypress", setQuery);
+
+function setQuery(e) {
+  if (e.keyCode == 13 || e.which == 13) {
+    getResults(searchbox.value);
+  }
+}
+function getResults(query) {
+  fetch(`${API.url}weather?q=${query}&units=metric&APPID=${API.key}`)
+    .then(function (weather) {
+      return weather.json();
+    })
+    .then(displayResults);
+}
+
+function displayResults(weather) {
+  //   console.log(weather);
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let today = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = today.toLocaleDateString();
+
+  let temp = document.querySelector(".current .temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)} <span>°C</span>`;
+
+  let weatherC = document.querySelector(".current .weather");
+  weatherC.innerText = weather.weather[0].description;
+
+  let hiLow = document.querySelector(".hi-low");
+  hiLow.innerText = `${weather.main.temp_min}°C / ${weather.main.temp_max}°C`;
+}
